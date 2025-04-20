c0412b4f93a649396a479625429246af240a0865
Edward Bruin <30287953+edwardbruin@users.noreply.github.com>
2025-04-21 00:49:17 +1000

    Update lcd_st7920.c

--- src/lcd_st7920.c
+++ src/lcd_st7920.c
@@ -1,300 +1,172 @@
-# Support for ST7920 (128x64 graphics) LCD displays
-#
-# Copyright (C) 2018  Kevin O'Connor <kevin@koconnor.net>
-#
-# This file may be distributed under the terms of the GNU GPLv3 license.
-import logging
-from .. import bus
-from . import font8x14
+// Commands for sending messages to an st7920 lcd driver
+//
+// Copyright (C) 2018  Kevin O'Connor <kevin@koconnor.net>
+//
+// This file may be distributed under the terms of the GNU GPLv3 license.
 
-BACKGROUND_PRIORITY_CLOCK = 0x7fffffff00000000
+#include "autoconf.h" // CONFIG_MACH_AVR
+#include "basecmd.h" // oid_alloc
+#include "board/gpio.h" // gpio_out_write
+#include "board/irq.h" // irq_poll
+#include "board/misc.h" // timer_from_us
+#include "command.h" // DECL_COMMAND
+#include "sched.h" // DECL_SHUTDOWN
 
-# Spec says 72us, but faster is possible in practice
-#ST7920_CMD_DELAY  = .000020
-#ST7920_SYNC_DELAY = .000045
+struct st7920 {
+    uint32_t last_cmd_time, sync_wait_ticks, cmd_wait_ticks;
+    struct gpio_out sclk, sid, cs;
+};
 
-ST7920_CMD_DELAY  = .000030
-ST7920_SYNC_DELAY = .000050
 
-TextGlyphs = { 'right_arrow': b'\x1a' }
-CharGlyphs = { 'degrees': bytearray(font8x14.VGA_FONT[0xf8]) }
+/****************************************************************
+ * Transmit functions
+ ****************************************************************/
 
-class DisplayBase:
-    def __init__(self):
-        # framebuffers
-        self.text_framebuffer = bytearray(b' '*64)
-        self.glyph_framebuffer = bytearray(128)
-        self.graphics_framebuffers = [bytearray(32) for i in range(32)]
-        self.all_framebuffers = [
-            # Text framebuffer
-            (self.text_framebuffer, bytearray(b'~'*64), 0x80),
-            # Glyph framebuffer
-            (self.glyph_framebuffer, bytearray(b'~'*128), 0x40),
-            # Graphics framebuffers
-            ] + [(self.graphics_framebuffers[i], bytearray(b'~'*32), i)
-                 for i in range(32)]
-        self.cached_glyphs = {}
-        self.icons = {}
-        
-        self.CacheTextAsGraph = [bytearray(32) for i in range(32)]
-        self.CacheGraphics = [bytearray(32) for i in range(32)]
-        
-    def flush(self):
-        # Find all differences in the framebuffers and send them to the chip
-        for new_data, old_data, fb_id in self.all_framebuffers:
-            if new_data == old_data:
-                continue
-            # Find the position of all changed bytes in this framebuffer
-            diffs = [[i, 1] for i, (n, o) in enumerate(zip(new_data, old_data))
-                     if n != o]
-            # Batch together changes that are close to each other
-            for i in range(len(diffs)-2, -1, -1):
-                pos, count = diffs[i]
-                nextpos, nextcount = diffs[i+1]
-                if pos + 5 >= nextpos and nextcount < 16:
-                    diffs[i][1] = nextcount + (nextpos - pos)
-                    del diffs[i+1]
-            # Transmit changes
-            for pos, count in diffs:
-                count += pos & 0x01
-                count += count & 0x01
-                pos = pos & ~0x01
-                chip_pos = pos >> 1
-                if fb_id < 0x40:
-                    # Graphics framebuffer update
-                    self.send([0x80 + fb_id, 0x80 + chip_pos], is_extended=True)
-                else:
-                    self.send([fb_id + chip_pos])
-                self.send(new_data[pos:pos+count], is_data=True)
-            old_data[:] = new_data
-    def init(self):
-        cmds = [0x24, # Enter extended mode
-                0x40, # Clear vertical scroll address
-                0x02, # Enable CGRAM access
-                0x26, # Enable graphics
-                0x22, # Leave extended mode
-                0x02, # Home the display
-                0x06, # Set positive update direction
-                0x0c] # Enable display and hide cursor
-        self.send(cmds)
-        self.flush()
-    def cache_glyph(self, glyph_name, base_glyph_name, glyph_id):
-        icon = self.icons.get(glyph_name)
-        base_icon = self.icons.get(base_glyph_name)
-        if icon is None or base_icon is None:
-            return
-        all_bits = zip(icon[0], icon[1], base_icon[0], base_icon[1])
-        for i, (ic1, ic2, b1, b2) in enumerate(all_bits):
-            x1, x2 = ic1 ^ b1, ic2 ^ b2
-            pos = glyph_id*32 + i*2
-            self.glyph_framebuffer[pos:pos+2] = [x1, x2]
-            self.all_framebuffers[1][1][pos:pos+2] = [x1 ^ 1, x2 ^ 1]
-        self.cached_glyphs[glyph_name] = (base_glyph_name, (0, glyph_id*2))
-    def set_glyphs(self, glyphs):
-        for glyph_name, glyph_data in glyphs.items():
-            icon = glyph_data.get('icon16x16')
-            if icon is not None:
-                self.icons[glyph_name] = icon
-        # Setup animated glyphs
-        self.cache_glyph('fan2', 'fan1', 0)
-        self.cache_glyph('bed_heat2', 'bed_heat1', 1)
+static uint32_t
+nsecs_to_ticks(uint32_t ns)
+{
+    return timer_from_us(ns * 1000) / 1000000;
+}
+
+static inline void
+ndelay(uint32_t nsecs)
+{
+    if (CONFIG_MACH_AVR)
+        // Slower MCUs don't require a delay
+        return;
+    uint32_t end = timer_read_time() + nsecs_to_ticks(nsecs);
+    while (timer_is_before(timer_read_time(), end))
+        irq_poll();
+}
+
+#define SYNC_CMD  0xf8
+#define SYNC_DATA 0xfa
+
+// Write eight bits to the st7920 via the serial interface
+static void
+st7920_xmit_byte(struct st7920 *s, uint8_t data)
+{
+    struct gpio_out sclk = s->sclk, sid = s->sid;
+    uint8_t i;
+    for (i=0; i<8; i++) {
+	gpio_out_write(sclk, 0);
+        if (data & 0x80) {
+            gpio_out_toggle(sid);
+            data = ~data;
+        }
+        ndelay(200);
+        gpio_out_toggle(sclk);
+        ndelay(200);
+        data <<= 1;
+    }
+}
+
+// Transmit a series of command bytes to the chip
+static void
+st7920_xmit(struct st7920 *s, uint8_t count, uint8_t *cmds)
+{
+    if (!count)
+        return;
+
+    // Send first byte (with longer delay)
+    uint32_t last_cmd_time = s->last_cmd_time, wait_ticks = s->sync_wait_ticks;
+    uint8_t cmd = *cmds++;
+    st7920_xmit_byte(s, cmd & 0xf0);
+    while (timer_read_time() - last_cmd_time < wait_ticks)
+        // Can't complete transfer until delay complete
+        irq_poll();      
+    st7920_xmit_byte(s, cmd << 4);
     
-    def upd_graph(self, x, y, lenX, lenY):
-        for i in range(y,y+lenY):
-            for c in range(x,x+lenX):
-                self.graphics_framebuffers[i][c] = self.CacheTextAsGraph[i][c] ^ self.CacheGraphics[i][c]
+    last_cmd_time = timer_read_time();
 
-    def write_text(self, x, y, data):
-        if x + len(data) > 16:
-            data = data[:16 - min(x, 16)]
-        
-        gfx_fb = 16 * y
-        if gfx_fb >= 32:
-            gfx_fb -= 32
-            x += 16  
-      
-        for i, asc in enumerate(data):
-            if type(asc) is str:
-                c=ord(asc)
-            else:
-                c = asc
-            if c > 6:
-                for r in range(16):
-                    self.CacheTextAsGraph[gfx_fb+r][x+i] = font8x14.VGA_FONT[c][r]
-#Comment out the next 4 lines if you don't want animation. (Reduces the load on the MCU)
-            else:
-                for r in range(16):
-                    self.CacheTextAsGraph[gfx_fb+r][x+i-1] = self.glyph_framebuffer[c*16+r*2]
-                    self.CacheTextAsGraph[gfx_fb+r][x+i] = self.glyph_framebuffer[c*16+r*2+1]
-#****************************************************************************************
-        self.upd_graph(x,gfx_fb,len(data),16)
+    // Send subsequent bytes
+    wait_ticks = s->cmd_wait_ticks;
+    while (--count) {
+        uint8_t cmd = *cmds++;
+        st7920_xmit_byte(s, cmd & 0xf0);
+        while (timer_read_time() - last_cmd_time < wait_ticks)
+            // Can't complete transfer until delay complete
+            irq_poll();        
+        st7920_xmit_byte(s, cmd << 4);        
 
-    
-    def write_graphics(self, x, y, data):
-        if x >= 16 or y >= 4 or len(data) != 16:
-            return
-        gfx_fb = y * 16
-        if gfx_fb >= 32:
-            gfx_fb -= 32
-            x += 16
-        for i, bits in enumerate(data):
-            self.CacheGraphics[gfx_fb + i][x] = bits
-            
-        self.upd_graph(x,gfx_fb,1,len(data))
-        
-    def write_glyph(self, x, y, glyph_name):
-        glyph_id = self.cached_glyphs.get(glyph_name)
-        if glyph_id is not None and x & 1 == 0:
-            # Render cached icon using character generator
-            glyph_name = glyph_id[0]
-            self.write_text(x, y, glyph_id[1])
-        icon = self.icons.get(glyph_name)
-        if icon is not None:
-            # Draw icon in graphics mode
-            self.write_graphics(x, y, icon[0])
-            self.write_graphics(x + 1, y, icon[1])
-            return 2
-        char = TextGlyphs.get(glyph_name)
-        if char is not None:
-            # Draw character
-            self.write_text(x, y, char)
-            return 1
-        font = CharGlyphs.get(glyph_name)
-        if font is not None:
-            # Draw single width character
-            self.write_graphics(x, y, font)
-            return 1
-        return 0
-    def clear(self):
-        self.text_framebuffer[:] = b' '*64
-        zeros = bytearray(32)
-        for gfb in self.graphics_framebuffers:
-            gfb[:] = zeros
-        for gfb in self.CacheGraphics:
-            gfb[:] = zeros
-        for gfb in self.CacheTextAsGraph:
-            gfb[:] = zeros
+        last_cmd_time = timer_read_time();
+    }
 
-	
-    def get_dimensions(self):
-        return (16, 4)
+    s->last_cmd_time = last_cmd_time;
+}
 
-# Display driver for stock ST7920 displays
-class ST7920(DisplayBase):
-    def __init__(self, config):
-        printer = config.get_printer()
-        # pin config
-        ppins = printer.lookup_object('pins')
-        pins = [ppins.lookup_pin(config.get(name + '_pin'))
-                for name in ['cs', 'sclk', 'sid']]
-        mcu = None
-        for pin_params in pins:
-            if mcu is not None and pin_params['chip'] != mcu:
-                raise ppins.error("st7920 all pins must be on same mcu")
-            mcu = pin_params['chip']
-        self.pins = [pin_params['pin'] for pin_params in pins]
-        # prepare send functions
-        self.mcu = mcu
-        self.oid = self.mcu.create_oid()
-        self.mcu.register_config_callback(self.build_config)
-        self.send_data_cmd = self.send_cmds_cmd = None
-        self.is_extended = False
-        # init display base
-        DisplayBase.__init__(self)
-    def build_config(self):
-        # configure send functions
-        self.mcu.add_config_cmd(
-            "config_st7920 oid=%u cs_pin=%s sclk_pin=%s sid_pin=%s"
-            " sync_delay_ticks=%d cmd_delay_ticks=%d" % (
-                self.oid, self.pins[0], self.pins[1], self.pins[2],
-                self.mcu.seconds_to_clock(ST7920_SYNC_DELAY),
-                self.mcu.seconds_to_clock(ST7920_CMD_DELAY)))
-        cmd_queue = self.mcu.alloc_command_queue()
-        self.send_cmds_cmd = self.mcu.lookup_command(
-            "st7920_send_cmds oid=%c cmds=%*s", cq=cmd_queue)
-        self.send_data_cmd = self.mcu.lookup_command(
-            "st7920_send_data oid=%c data=%*s", cq=cmd_queue)
-    def send(self, cmds, is_data=False, is_extended=False):
-        cmd_type = self.send_cmds_cmd
-        if is_data:
-            cmd_type = self.send_data_cmd
-        elif self.is_extended != is_extended:
-            add_cmd = 0x22
-            if is_extended:
-                add_cmd = 0x26
-            cmds = [add_cmd] + cmds
-            self.is_extended = is_extended
-        cmd_type.send([self.oid, cmds], reqclock=BACKGROUND_PRIORITY_CLOCK)
-        #logging.debug("st7920 %d %s", is_data, repr(cmds))
 
-# Helper code for toggling the en pin on startup
-class EnableHelper:
-    def __init__(self, pin_desc, spi):
-        self.en_pin = bus.MCU_bus_digital_out(spi.get_mcu(), pin_desc,
-                                              spi.get_command_queue())
-    def init(self):
-        mcu = self.en_pin.get_mcu()
-        curtime = mcu.get_printer().get_reactor().monotonic()
-        print_time = mcu.estimated_print_time(curtime)
-        # Toggle enable pin
-        minclock = mcu.print_time_to_clock(print_time + .100)
-        self.en_pin.update_digital_out(0, minclock=minclock)
-        minclock = mcu.print_time_to_clock(print_time + .200)
-        self.en_pin.update_digital_out(1, minclock=minclock)
-        # Force a delay to any subsequent commands on the command queue
-        minclock = mcu.print_time_to_clock(print_time + .300)
-        self.en_pin.update_digital_out(1, minclock=minclock)
+/****************************************************************
+ * Interface
+ ****************************************************************/
 
-# Display driver for displays that emulate the ST7920 in software.
-# These displays rely on the CS pin to be toggled in order to initialize the
-# SPI correctly. This display driver uses a software SPI with an unused pin
-# as the MISO pin.
-class EmulatedST7920(DisplayBase):
-    def __init__(self, config):
-        # create software spi
-        ppins = config.get_printer().lookup_object('pins')
-        sw_pin_names = ['spi_software_%s_pin' % (name,)
-                        for name in ['miso', 'mosi', 'sclk']]
-        sw_pin_params = [ppins.lookup_pin(config.get(name), share_type=name)
-                         for name in sw_pin_names]
-        mcu = None
-        for pin_params in sw_pin_params:
-            if mcu is not None and pin_params['chip'] != mcu:
-                raise ppins.error("%s: spi pins must be on same mcu" % (
-                    config.get_name(),))
-            mcu = pin_params['chip']
-        sw_pins = tuple([pin_params['pin'] for pin_params in sw_pin_params])
-        speed = config.getint('spi_speed', 1000000, minval=100000)
-        self.spi = bus.MCU_SPI(mcu, None, None, 0, speed, sw_pins)
-        # create enable helper
-        self.en_helper = EnableHelper(config.get("en_pin"), self.spi)
-        self.en_set = False
-        # init display base
-        self.is_extended = False
-        DisplayBase.__init__(self)
-    def send(self, cmds, is_data=False, is_extended=False):
-        # setup sync byte and check for exten mode switch
-        sync_byte = 0xfa
-        if not is_data:
-            sync_byte = 0xf8
-            if self.is_extended != is_extended:
-                add_cmd = 0x22
-                if is_extended:
-                    add_cmd = 0x26
-                cmds = [add_cmd] + cmds
-                self.is_extended = is_extended
-        # copy data to ST7920 data format
-        spi_data = [0] * (2 * len(cmds) + 1)
-        spi_data[0] = sync_byte
-        i = 1
-        for b in cmds:
-            spi_data[i] = b & 0xF0
-            spi_data[i + 1] = (b & 0x0F) << 4
-            i = i + 2
-        # check if enable pin has been set
-        if not self.en_set:
-            self.en_helper.init()
-            self.en_set = True
-        # send data
-        self.spi.spi_send(spi_data, reqclock=BACKGROUND_PRIORITY_CLOCK)
-        #logging.debug("st7920 %s", repr(spi_data))
+void
+command_config_st7920(uint32_t *args)
+{
+    struct st7920 *s = oid_alloc(args[0], command_config_st7920, sizeof(*s));
+    s->sclk = gpio_out_setup(args[2], 0);
+    s->sid = gpio_out_setup(args[3], 0);
+    //gpio_out_setup(args[1], 1);
+    s->cs = gpio_out_setup(args[1], 0);	
+
+    if (!CONFIG_HAVE_STRICT_TIMING) {
+        s->sync_wait_ticks = args[4];
+        s->cmd_wait_ticks = args[5];
+        return;
+    }
+
+    // Calibrate cmd_wait_ticks
+    st7920_xmit_byte(s, SYNC_CMD);
+    st7920_xmit_byte(s, 0x20);
+    irq_disable();
+    uint32_t start = timer_read_time();
+    st7920_xmit_byte(s, 0x00);
+    uint32_t end = timer_read_time();
+    irq_enable();
+    s->last_cmd_time = end;
+    uint32_t diff = end - start, sync_delay_ticks = args[4];
+    if (sync_delay_ticks > diff)
+        s->sync_wait_ticks = sync_delay_ticks - diff;
+    uint32_t cmd_delay_ticks = args[5];
+    if (cmd_delay_ticks > diff)
+        s->cmd_wait_ticks = cmd_delay_ticks - diff;
+}
+DECL_COMMAND(command_config_st7920,
+             "config_st7920 oid=%c cs_pin=%u sclk_pin=%u sid_pin=%u"
+             " sync_delay_ticks=%u cmd_delay_ticks=%u");
+
+void
+command_st7920_send_cmds(uint32_t *args)
+{
+    struct st7920 *s = oid_lookup(args[0], command_config_st7920);
+    gpio_out_write(s->cs,1);
+    st7920_xmit_byte(s, SYNC_CMD);
+    uint8_t len = args[1], *cmds = command_decode_ptr(args[2]);
+    st7920_xmit(s, len, cmds);
+    gpio_out_write(s->cs,0);
+}
+DECL_COMMAND(command_st7920_send_cmds, "st7920_send_cmds oid=%c cmds=%*s");
+
+void
+command_st7920_send_data(uint32_t *args)
+{
+    struct st7920 *s = oid_lookup(args[0], command_config_st7920);
+    gpio_out_write(s->cs,1);
+    st7920_xmit_byte(s, SYNC_DATA);
+    uint8_t len = args[1], *data = command_decode_ptr(args[2]);
+    st7920_xmit(s, len, data);
+    gpio_out_write(s->cs,0);
+}
+DECL_COMMAND(command_st7920_send_data, "st7920_send_data oid=%c data=%*s");
+
+void
+st7920_shutdown(void)
+{
+    uint8_t i;
+    struct st7920 *s;
+    foreach_oid(i, s, command_config_st7920) {
+        gpio_out_write(s->sclk, 0);
+        gpio_out_write(s->sid, 0);
+    }
+}
+DECL_SHUTDOWN(st7920_shutdown);
